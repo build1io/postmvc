b1dba369eb67a5e0126eec715714b1f73abfcb52
George Markosian <georgemarkosian@gmail.com>
2021-02-16 23:22:43 +0200

    UI controls binding compile time validation implemented.

--- Extensions/Unity/Modules/Popup/PopupConfig.cs
+++ Extensions/Unity/Modules/Popup/PopupConfig.cs
@@ -1,3 +1,5 @@
+using Build1.PostMVC.Extensions.MVCS.Mediation;
+using Build1.PostMVC.Extensions.Unity.Mediation;
 using Build1.PostMVC.Extensions.Unity.Modules.Device;
 using Build1.PostMVC.Extensions.Unity.Modules.UI;
 
@@ -20,14 +22,16 @@ namespace Build1.PostMVC.Extensions.Unity.Modules.Popup
         public PopupConfig(DevicePlatform platform, DeviceType deviceType, int assetBundleId, string prefabName, int appLayerId) : base(platform, deviceType, assetBundleId, prefabName, appLayerId)
         {
         }
-        
-        public new PopupConfig AddBinding<V, M>()
+
+        public new PopupConfig AddBinding<V, M>() where V : UnityView
+                                                  where M : Mediator
         {
             base.AddBinding<V, M>();
             return this;
         }
-        
-        public new PopupConfig AddBinding<V, I, M>()
+
+        public new PopupConfig AddBinding<V, I, M>() where V : UnityView, I
+                                                     where M : Mediator
         {
             base.AddBinding<V, I, M>();
             return this;
--- Extensions/Unity/Modules/Screen/ScreenConfig.cs
+++ Extensions/Unity/Modules/Screen/ScreenConfig.cs
@@ -1,3 +1,5 @@
+using Build1.PostMVC.Extensions.MVCS.Mediation;
+using Build1.PostMVC.Extensions.Unity.Mediation;
 using Build1.PostMVC.Extensions.Unity.Modules.Device;
 using Build1.PostMVC.Extensions.Unity.Modules.UI;
 
@@ -8,26 +10,28 @@ namespace Build1.PostMVC.Extensions.Unity.Modules.Screen
         public ScreenConfig(int assetBundleId, string prefabName, int appLayerId) : base(assetBundleId, prefabName, appLayerId)
         {
         }
-        
+
         public ScreenConfig(DevicePlatform platform, int assetBundleId, string prefabName, int appLayerId) : base(platform, assetBundleId, prefabName, appLayerId)
         {
         }
-        
+
         public ScreenConfig(DeviceType deviceType, int assetBundleId, string prefabName, int appLayerId) : base(deviceType, assetBundleId, prefabName, appLayerId)
         {
         }
-        
+
         public ScreenConfig(DevicePlatform platform, DeviceType deviceType, int assetBundleId, string prefabName, int appLayerId) : base(platform, deviceType, assetBundleId, prefabName, appLayerId)
         {
         }
-        
-        public new ScreenConfig AddBinding<V, M>()
+
+        public new ScreenConfig AddBinding<V, M>() where V : UnityView
+                                                   where M : Mediator
         {
             base.AddBinding<V, M>();
             return this;
         }
-        
-        public new ScreenConfig AddBinding<V, I, M>()
+
+        public new ScreenConfig AddBinding<V, I, M>() where V : UnityView, I
+                                                      where M : Mediator
         {
             base.AddBinding<V, I, M>();
             return this;
