130f385603333cd9ae526bd6477c3a3416820b45
George Markosian <georgemarkosian@gmail.com>
2021-03-19 15:38:03 +0200

    Editor menus restructured.

--- Extensions/Unity/Modules/App/Editor/BuildNumberMenu.cs
+++ Extensions/Unity/Modules/App/Editor/BuildNumberMenu.cs
@@ -4,21 +4,21 @@ using UnityEditor;
 
 namespace Build1.PostMVC.Extensions.Unity.Modules.App.Editor
 {
-    public static class BuildMenu
+    public static class BuildNumberMenu
     {
-        [MenuItem("Build/Increment Build Number", false, 10)]
+        [MenuItem("PostMVC/Build Number/Increment", false, 10)]
         public static void IncrementBuildNumber()
         {
             BuildNumberProcessor.IncrementBuildNumber();
         }
 
-        [MenuItem("Build/Update Build Number", false, 30)]
+        [MenuItem("PostMVC/Build Number/Update", false, 11)]
         public static void UpdateBuildNumber()
         {
             BuildNumberProcessor.UpdateBuildNumberFromProjectSettings();
         }
 
-        [MenuItem("Build/Reset Build Number", false, 50)]
+        [MenuItem("PostMVC/Build Number/Reset", false, 50)]
         public static void ResetBuildNumber()
         {
             BuildNumberProcessor.SetBuildNumber(0);
--- Extensions/Unity/Modules/Assets/Editor/AssetBundlesMenu.cs
+++ Extensions/Unity/Modules/Assets/Editor/AssetBundlesMenu.cs
@@ -6,56 +6,56 @@ namespace Build1.PostMVC.Extensions.Unity.Modules.Assets.Editor
 {
     public static class AssetBundlesMenu
     {
-        [MenuItem("Asset Bundles/Build", false, 10)]
+        [MenuItem("PostMVC/Asset Bundles/Build", false, 10)]
         public static void Build()
         {
             AssetBundlesBuilder.Build(EditorUserBuildSettings.activeBuildTarget, BuildAssetBundleOptions.StrictMode);
         }
         
-        [MenuItem("Asset Bundles/Rebuild", false, 11)]
+        [MenuItem("PostMVC/Asset Bundles/Rebuild", false, 11)]
         public static void Rebuild()
         {
             Clear();
             Build();
         }
         
-        [MenuItem("Asset Bundles/Build Android", false, 50)]
+        [MenuItem("PostMVC/Asset Bundles/Build Android", false, 50)]
         public static void BuildAndroid()
         {
             AssetBundlesBuilder.Build(BuildTarget.Android, BuildAssetBundleOptions.StrictMode);
         }
         
-        [MenuItem("Asset Bundles/Build IOS", false, 51)]
+        [MenuItem("PostMVC/Asset Bundles/Build IOS", false, 51)]
         public static void BuildIOS()
         {
             AssetBundlesBuilder.Build(BuildTarget.iOS, BuildAssetBundleOptions.StrictMode);
         }
         
-        [MenuItem("Asset Bundles/Build OSX", false, 100)]
+        [MenuItem("PostMVC/Asset Bundles/Build OSX", false, 100)]
         public static void BuildOSX()
         {
             AssetBundlesBuilder.Build(BuildTarget.StandaloneOSX, BuildAssetBundleOptions.StrictMode);
         }
 
-        [MenuItem("Asset Bundles/Build Windows", false, 101)]
+        [MenuItem("PostMVC/Asset Bundles/Build Windows", false, 101)]
         public static void BuildWindows()
         {
             AssetBundlesBuilder.Build(BuildTarget.StandaloneWindows, BuildAssetBundleOptions.StrictMode);
         }
         
-        [MenuItem("Asset Bundles/Build Windows 64", false, 102)]
+        [MenuItem("PostMVC/Asset Bundles/Build Windows 64", false, 102)]
         public static void BuildWindows64()
         {
             AssetBundlesBuilder.Build(BuildTarget.StandaloneWindows64, BuildAssetBundleOptions.StrictMode);
         }
         
-        [MenuItem("Asset Bundles/Build WebGL", false, 150)]
+        [MenuItem("PostMVC/Asset Bundles/Build WebGL", false, 150)]
         public static void BuildWebGL()
         {
             AssetBundlesBuilder.Build(BuildTarget.WebGL, BuildAssetBundleOptions.StrictMode);
         }
 
-        [MenuItem("Asset Bundles/Clear", false, 200)]
+        [MenuItem("PostMVC/Asset Bundles/Clear", false, 200)]
         public static void Clear()
         {
             AssetBundlesBuilder.Clear();
