83c4256cd4c75232a41cd24b0573144842092c53
George Markosian <georgemarkosian@gmail.com>
2021-03-22 22:42:04 +0200

    Default UI control behavior added.

--- Extensions/Unity/Modules/UI/UIControlBehavior.cs
+++ Extensions/Unity/Modules/UI/UIControlBehavior.cs
@@ -5,11 +5,15 @@ namespace Build1.PostMVC.Extensions.Unity.Modules.UI
     [Flags]
     public enum UIControlBehavior
     {
+        /// Control will not be pre instantiated. 
+        /// Control will not be destroyed on deactivation.
+        Default = 1 << 0,
+        
         /// Control will be pre instantiated on initialization.
         /// UIControlsController.Initialize method must be called. 
-        PreInstantiate = 1 << 0,
+        PreInstantiate = 1 << 1,
         
         /// Control will be destroyed on deactivation.
-        DestroyOnDeactivation = 1 << 1
+        DestroyOnDeactivation = 1 << 2
     }
 }
