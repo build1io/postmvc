70f675400368c08860c698aab858ac6bdb17c721
George Markosian <georgemarkosian@gmail.com>
2021-03-31 20:50:36 +0300

    PersistentDataPath added to AppController. Workaround for Android implemented.

--- Extensions/Unity/Modules/App/IAppController.cs
+++ Extensions/Unity/Modules/App/IAppController.cs
@@ -2,16 +2,13 @@ namespace Build1.PostMVC.Extensions.Unity.Modules.App
 {
     public interface IAppController
     {
-        string Version     { get; }
-        int    BuildNumber { get; }
+        string PersistentDataPath { get; }
+        string Version            { get; }
+        int    BuildNumber        { get; }
 
         bool IsPaused  { get; }
         bool IsFocused { get; }
 
-        void OpenUrl(string url);
-        void MailTo(string email, string subject, string body);
-        void CopyToClipboard(string content);
-        
         void Restart();
     }
 }
--- Extensions/Unity/Modules/App/Impl/AppController.cs
+++ Extensions/Unity/Modules/App/Impl/AppController.cs
@@ -4,7 +4,6 @@ using Build1.PostMVC.Extensions.MVCS.Injection;
 using Build1.PostMVC.Extensions.Unity.Modules.Agents;
 using Build1.PostMVC.Extensions.Unity.Modules.Logging;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 using Logger = Build1.PostMVC.Extensions.Unity.Modules.Logging.Logger;
 using ILogger = Build1.PostMVC.Extensions.Unity.Modules.Logging.ILogger;
@@ -20,8 +19,9 @@ namespace Build1.PostMVC.Extensions.Unity.Modules.App.Impl
         [Inject]                   public  IContext          Context          { get; set; }
         [Inject]                   private IAgentsController AgentsController { get; set; }
 
-        public string Version     => Application.version;
-        public int    BuildNumber { get; private set; }
+        public string PersistentDataPath { get; private set; }
+        public string Version            => Application.version;
+        public int    BuildNumber        { get; private set; }
 
         public bool IsPaused  { get; private set; }
         public bool IsFocused { get; private set; }
@@ -32,7 +32,9 @@ namespace Build1.PostMVC.Extensions.Unity.Modules.App.Impl
         [PostConstruct]
         private void PostConstruct()
         {
+            PersistentDataPath = GetPersistentDataPath(PathAttribute.Internal | PathAttribute.Persistent | PathAttribute.Canonical);
             BuildNumber = GetBuildNumber();
+            
             _mainSceneName = GetMainSceneName();
 
             _agent = AgentsController.Create<AppAgent>();
@@ -56,25 +58,6 @@ namespace Build1.PostMVC.Extensions.Unity.Modules.App.Impl
          * Public.
          */
 
-        public void OpenUrl(string url)
-        {
-            Application.OpenURL(url);
-        }
-
-        public void MailTo(string email, string subject, string body)
-        {
-            static string Escape(string str)
-            {
-                return UnityWebRequest.EscapeURL(str).Replace("+","%20");
-            }
-            Application.OpenURL($"mailto:{email}?subject={Escape(subject)}&body={Escape(body)}");
-        }
-
-        public void CopyToClipboard(string content)
-        {
-            GUIUtility.systemCopyBuffer = content;
-        }
-        
         public void Restart()
         {
             // Signalling that app is about to restart.
@@ -92,6 +75,64 @@ namespace Build1.PostMVC.Extensions.Unity.Modules.App.Impl
          * Private.
          */
 
+        private string GetPersistentDataPath(PathAttribute pathAttribute)
+        {
+            #if UNITY_ANDROID && !UNITY_EDITOR
+
+            // Automatic => /storage/emulated/0/Android/data/[PACKAGE_NAME]/files: OK
+
+            // Internal, Persistent, Absolute => /data/user/0/[PACKAGE_NAME]/files: OK
+            // Internal, Persistent, Default => /data/user/0/[PACKAGE_NAME]/files: OK
+            // Internal, Persistent, Canonical => /data/data/[PACKAGE_NAME]/files: OK
+
+            // Internal, Cached, Absolute => /data/user/0/[PACKAGE_NAME]/cache: OK
+            // Internal, Cached, Default => /data/user/0/[PACKAGE_NAME]/cache: OK
+            // Internal, Cached, Canonical => /data/data/[PACKAGE_NAME]/cache: OK
+
+            // External, Persistent, Absolute => /storage/emulated/0/Android/data/[PACKAGE_NAME]/files: OK
+            // External, Persistent, Default => /storage/emulated/0/Android/data/[PACKAGE_NAME]/files: OK
+            // External, Persistent, Canonical => /storage/emulated/0/Android/data/[PACKAGE_NAME]/files: OK
+
+            // External, Cached, Absolute => /storage/emulated/0/Android/data/[PACKAGE_NAME]/cache: OK
+            // External, Cached, Default => /storage/emulated/0/Android/data/[PACKAGE_NAME]/cache: OK
+            // External, Cached, Canonical => /storage/emulated/0/Android/data/[PACKAGE_NAME]/cache: OK
+
+            if (pathAttribute == PathAttribute.Automatic)
+                return Application.persistentDataPath;
+            
+            var activityClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            var activity = activityClass.GetStatic<AndroidJavaObject>("currentActivity");
+
+            var endMethod = string.Empty;
+            if ((pathAttribute & PathAttribute.Absolute) == PathAttribute.Absolute)
+                endMethod = "getAbsolutePath";
+            else if ((pathAttribute & PathAttribute.Canonical) == PathAttribute.Canonical)
+                endMethod = "getCanonicalPath";
+            else if ((pathAttribute & PathAttribute.Default) == PathAttribute.Default)
+                endMethod = "getPath";
+
+            if ((pathAttribute & PathAttribute.Internal) == PathAttribute.Internal && (pathAttribute & PathAttribute.Persistent) == PathAttribute.Persistent)
+                return activity.Call<AndroidJavaObject>("getFilesDir").Call<string>(endMethod);
+            
+            if ((pathAttribute & PathAttribute.Internal) == PathAttribute.Internal && (pathAttribute & PathAttribute.Cached) == PathAttribute.Cached)
+                return activity.Call<AndroidJavaObject>("getCacheDir").Call<string>(endMethod);
+            
+            if ((pathAttribute & PathAttribute.External) == PathAttribute.External && (pathAttribute & PathAttribute.Persistent) == PathAttribute.Persistent)
+                return activity.Call<AndroidJavaObject>("getExternalFilesDir", (object)null).Call<string>(endMethod);
+            
+            if ((pathAttribute & PathAttribute.External) == PathAttribute.External && (pathAttribute & PathAttribute.Cached) == PathAttribute.Cached)
+                return activity.Call<AndroidJavaObject>("getExternalCacheDir").Call<string>(endMethod);
+            
+            throw new ArgumentOutOfRangeException(nameof(pathAttribute), pathAttribute, null);
+            
+            #else
+            
+            // PathAttributes are applicable only to Android platform at the moment.
+            return Application.persistentDataPath;
+            
+            #endif
+        }
+        
         private string GetMainSceneName()
         {
             return System.IO.Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(0));
