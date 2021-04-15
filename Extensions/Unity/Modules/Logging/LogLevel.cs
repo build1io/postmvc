d23810c26d4b1943c7de34f01b733972a2dd3596
George Markosian <georgemarkosian@gmail.com>
2021-04-15 21:18:39 +0300

    LogLevel syntax update.

--- Extensions/Unity/Modules/Logging/LogLevel.cs
+++ Extensions/Unity/Modules/Logging/LogLevel.cs
@@ -6,9 +6,9 @@ namespace Build1.PostMVC.Extensions.Unity.Modules.Logging
     public enum LogLevel
     {
         None    = 0,
-        Debug   = 1,
-        Warning = 2,
-        Error   = 4,
+        Debug   = 1 << 0,
+        Warning = 1 << 1,
+        Error   = 1 << 2,
 
         All        = Error | Warning | Debug,
         Verbose    = Error | Warning,
