eaf930835b4b5d06a9d87214580d2426970216e5
George Markosian <georgemarkosian@gmail.com>
2021-05-24 18:32:15 +0300

    TimeExtension renamed to DateTimeExtension.

--- Utils/Extensions/DateTimeExtensions.cs
+++ Utils/Extensions/DateTimeExtensions.cs
@@ -2,8 +2,18 @@ using System;
 
 namespace Build1.PostMVC.Utils.Extensions
 {
-    public static class TimeExtensions
+    public static class DateTimeExtensions
     {
+        public static long ToUnixTimestamp(this DateTime dateTime)
+        {
+            return (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+        }
+
+        public static DateTime FromUnixTimestamp(this long timestamp)
+        {
+            return new DateTime(1970, 1, 1).AddSeconds(timestamp);
+        }
+        
         public static string FormatAsSeconds(this int seconds)
         {
             var timeSpan = TimeSpan.FromSeconds(seconds);
