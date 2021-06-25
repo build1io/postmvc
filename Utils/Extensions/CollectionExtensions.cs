fac7b326c07734100708421b8c410c7849f89039
George Markosian <georgemarkosian@gmail.com>
2021-06-25 20:49:44 +0300

    GetRandom methods renamed to just Random.

--- Utils/Extensions/CollectionExtensions.cs
+++ Utils/Extensions/CollectionExtensions.cs
@@ -7,12 +7,12 @@ namespace Build1.PostMVC.Utils.Extensions
     {
         private static readonly Random _random = new Random();
         
-        public static T GetRandom<T>(this T[] list)
+        public static T Random<T>(this T[] list)
         {
             return list.Length == 0 ? default : list[_random.Next(list.Length)];
         }
         
-        public static T GetRandom<T>(this IList<T> list)
+        public static T Random<T>(this IList<T> list)
         {
             return list.Count == 0 ? default : list[_random.Next(list.Count)];
         }
