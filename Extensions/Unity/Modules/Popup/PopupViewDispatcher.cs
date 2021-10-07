14ebd25b87741ea6e1d3d8dd05ea4716b9f5a481
George Markosian <georgemarkosian@gmail.com>
2021-10-07 18:58:35 +0300

    Popup view close method made virtual.

--- Extensions/Unity/Modules/Popup/PopupViewDispatcher.cs
+++ Extensions/Unity/Modules/Popup/PopupViewDispatcher.cs
@@ -16,7 +16,7 @@ namespace Build1.PostMVC.Extensions.Unity.Modules.Popup
             Popup = popup;
         }
 
-        public void Close()
+        public virtual void Close()
         {
             PopupController.Close(Popup);
         }
