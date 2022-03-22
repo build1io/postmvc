49424b4e9c8381e834abfc49828b223cb903b9de
George Markosian <georgemarkosian@gmail.com>
2022-03-22 15:11:20 +0100

    LogMetadata included to prevent Unity mapping issues.

---
+++ Extensions/Unity/Modules/Popups/Animation/PopupAnimationsScale2.cs
@@ -0,0 +1,17 @@
+using DG.Tweening;
+
+namespace Build1.PostMVC.Extensions.Unity.Modules.Popups.Animation
+{
+    public sealed class PopupAnimationsScale2 : PopupAnimation
+    {
+        public override void AnimateShow(IPopupView view, TweenCallback onComplete)
+        {
+            
+        }
+
+        public override void AnimateHide(IPopupView view, TweenCallback onComplete)
+        {
+            
+        }
+    }
+}
