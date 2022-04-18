60f8ef8a09cf269a2d15bcab7ae1a665be7a0738
George Markosian <georgemarkosian@gmail.com>
2022-04-18 12:24:30 +0200

    Notifications module moved to another sub-module.

--- Extensions/Unity/Modules/Notifications/NotificationsEvent.cs
+++
@@ -1,9 +0,0 @@
-using Build1.PostMVC.Extensions.MVCS.Events;
-
-namespace Build1.PostMVC.Extensions.Unity.Modules.Notifications
-{
-    public static class NotificationsEvent
-    {
-        public static readonly Event Initialized = new Event();
-    }
-}
