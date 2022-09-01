04621bcb6f941d17f23b01919e8806572214a77b
George Markosian <georgemarkosian@gmail.com>
2022-09-01 17:26:24 +0200

    Structure update.

--- Extensions/MVCS/Mediation/IMediator.cs
+++
@@ -1,6 +0,0 @@
-namespace Build1.PostMVC.Core.Extensions.MVCS.Mediation
-{
-    public interface IMediator
-    {
-    }
-}
--- Extensions/MVCS/Mediation/IViewWithDispatcher.cs
+++
@@ -1,8 +0,0 @@
-using Build1.PostMVC.Core.Extensions.MVCS.Events;
-
-namespace Build1.PostMVC.Core.Extensions.MVCS.Mediation
-{
-    public interface IViewWithDispatcher : IView, IEventDispatcher
-    {
-    }
-}
